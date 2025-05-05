23470fb48c16adc01183832df7b594c4ad5f7189
Adid Al Mahamud Shazid <adidalmahamud@iut-dhaka.edu>
2025-05-05 12:45:06 +0600

    b

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const port = process.env.PORT || 3001;
 
 const app = express();
 
+const path = require("path");
+
+app.use(express.static("./client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 app.use(express.json());
 app.use(cors());
 
